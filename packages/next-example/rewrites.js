41d42248bc6090f3025c91f5774dfbe19ad9b437
Ian-potter <Ian@portkey.finance>
2023-04-26 01:10:24 +0800

    feat: 🎸 add dependencies

--- packages/next-example/rewrites.js
+++ packages/next-example/rewrites.js
@@ -1,7 +1,9 @@
-module.exports = [
-    { source: '/api/:path*', destination: 'https://localtest-applesign.portkey.finance/api/:path*' },
+const rewrite = [
+    { source: '/api/:path*', destination: 'https://did-portkey.portkey.finance/api/:path*' },
     {
-        source: '/AElfIndexer_DApp/:path*',
-        destination: 'http://192.168.67.172:8083/AElfIndexer_DApp/:path*'
-    }
+        source: '/AElfIndexer_DApp/PortKeyIndexerCASchema/:path*',
+        destination: 'https://dapp-portkey.portkey.finance/Portkey_DID/PortKeyIndexerCASchema/:path*',
+    },
 ];
+
+module.exports = rewrite;
