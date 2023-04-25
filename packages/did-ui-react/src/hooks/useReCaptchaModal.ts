27d8f693b9cde56cf3993ddb0b23600d15004072
Ian-potter <Ian@portkey.finance>
2023-04-25 22:18:06 +0800

    v0.0.1-alpha.6.3

--- packages/did-ui-react/src/hooks/useReCaptchaModal.ts
+++ packages/did-ui-react/src/hooks/useReCaptchaModal.ts
@@ -1,29 +1,28 @@
 import { useCallback } from 'react';
-import { handleErrorMessage } from '../utils';
-import { setReCaptchaModal } from '../utils/reCaptcha';
-import useReCaptcha from './useReCaptcha';
+// import { handleErrorMessage } from '../utils';
+// import { setReCaptchaModal } from '../utils/reCaptcha';
+// import useReCaptcha from './useReCaptcha';
 export default function useReCaptchaModal() {
-  const reCaptchaInfo = useReCaptcha();
+  // const reCaptchaInfo = useReCaptcha();
 
-  return useCallback(
-    async (open?: boolean) => {
-      try {
-        if (open && reCaptchaInfo?.customReCaptchaHandler) {
-          const info = await reCaptchaInfo.customReCaptchaHandler();
-          if (info.type === 'success') return info;
-          throw info;
-        } else {
-          const info = await setReCaptchaModal(open);
-          if (info.type === 'success') return info;
-          throw info;
-        }
-      } catch (e: any) {
-        if (e.type === 'cancel') throw handleErrorMessage(e, 'User Cancel');
-        if (e.type === 'error') throw handleErrorMessage(e, 'ReCaptcha error');
-        if (e.type === 'expire') throw handleErrorMessage(e, 'ReCaptcha expire');
-        throw e;
-      }
-    },
-    [reCaptchaInfo],
-  );
+  return useCallback(async (open?: boolean) => {
+    console.log(open);
+    return { type: 'success', message: 'not use' };
+    // try {
+    //   if (open && reCaptchaInfo?.customReCaptchaHandler) {
+    //     const info = await reCaptchaInfo.customReCaptchaHandler();
+    //     if (info.type === 'success') return info;
+    //     throw info;
+    //   } else {
+    //     const info = await setReCaptchaModal(open);
+    //     if (info.type === 'success') return info;
+    //     throw info;
+    //   }
+    // } catch (e: any) {
+    //   if (e.type === 'cancel') throw handleErrorMessage(e, 'User Cancel');
+    //   if (e.type === 'error') throw handleErrorMessage(e, 'ReCaptcha error');
+    //   if (e.type === 'expire') throw handleErrorMessage(e, 'ReCaptcha expire');
+    //   throw e;
+    // }
+  }, []);
 }
