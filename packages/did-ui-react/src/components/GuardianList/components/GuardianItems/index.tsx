a0f46ed7bbbf9825b09645a028c55ffaa1ffcd57
Ian-potter <Ian@portkey.finance>
2023-05-15 15:52:52 +0800

    fix: 🐛 change style and export login Modal

--- packages/did-ui-react/src/components/GuardianList/components/GuardianItems/index.tsx
+++ packages/did-ui-react/src/components/GuardianList/components/GuardianItems/index.tsx
@@ -39,17 +39,17 @@ function GuardianItems({
     switch (guardian.guardianType) {
       case 'Email':
       case 'Phone':
-        return <div className="account-text">{guardian.identifier || guardian.identifierHash}</div>;
+        return <div className="account-text account-text-one-row">{guardian.identifier}</div>;
       case 'Google':
         return (
-          <div className="account-text">
+          <div className="account-text account-text-two-row">
             <div className="name">{guardian.firstName}</div>
             <div className="detail">{guardian.thirdPartyEmail}</div>
           </div>
         );
       case 'Apple':
         return (
-          <div className="account-text">
+          <div className="account-text account-text-two-row">
             <div className="name">{guardian.firstName}</div>
             <div className="detail">{guardian.isPrivate ? '******' : guardian.thirdPartyEmail}</div>
           </div>
