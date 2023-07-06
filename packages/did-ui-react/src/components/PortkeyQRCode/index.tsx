94a993a6b5237f616f5cabb8fe906c56a9173a8d
Ian-potter <Ian@portkey.finance>
2023-07-06 15:22:21 +0800

    fix: 🐛 qr code data too large

--- packages/did-ui-react/src/components/PortkeyQRCode/index.tsx
+++ packages/did-ui-react/src/components/PortkeyQRCode/index.tsx
@@ -1,15 +1,17 @@
 import { QRCode, IProps } from 'react-qrcode-logo';
 import PortkeyQR from './PortkeyQR.svg';
+import './index.less';
 
 export default function PortkeyQRCode({ value, ...props }: IProps) {
   return (
     <QRCode
+      id="portkey-qr-code"
       value={value}
-      size={200}
+      size={400}
       quietZone={0}
       logoImage={PortkeyQR}
-      logoWidth={40}
-      logoHeight={40}
+      logoWidth={80}
+      logoHeight={80}
       qrStyle={'squares'}
       eyeRadius={{ outer: 7, inner: 4 }}
       ecLevel={'L'}
