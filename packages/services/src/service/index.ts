e74ca66845a61d2febec13a35b15803ca2969504
thomas-portkey <thomas@portkey.finance>
2023-07-28 18:41:56 +0800

    feat: 🎸 add assets service

--- packages/services/src/service/index.ts
+++ packages/services/src/service/index.ts
@@ -2,3 +2,5 @@ export * from './communityRecovery';
 export * from './search';
 export * from './verification';
 export * from './connect';
+export * from './ramp';
+export * from './assets';
---
+++ packages/services/src/types/assets.ts
@@ -0,0 +1,91 @@
+export type ITokenItemResponse = {
+  decimals: number;
+  symbol: string;
+  tokenContractAddress: string;
+  balance: string;
+  chainId: string;
+  balanceInUsd: string;
+  imageUrl: string;
+  price: number;
+};
+
+export type FetchAccountTokenListParams = {
+  skipCount?: number;
+  maxResultCount?: number;
+  caAddresses: string[];
+  caAddressInfos: { chainId: string; caAddress: string }[];
+};
+export type FetchAccountTokenListResult = {
+  data: ITokenItemResponse[];
+  totalRecordCount: number;
+};
+
+export type GetSymbolImagesParams = {} | undefined;
+export type GetSymbolImagesResult = {
+  symbolImages: { [symbol: string]: string };
+};
+
+export type INftCollection = {
+  chainId: string;
+  collectionName: string;
+  imageUrl: string;
+  itemCount: number;
+  symbol: string;
+};
+export type FetchAccountNftCollectionListParams = {
+  caAddresses: string[];
+  skipCount: number;
+  maxResultCount?: number;
+  caAddressInfos: { chainId: string; caAddress: string }[];
+  width: number;
+  height: number;
+};
+export type FetchAccountNftCollectionListResult = {
+  data: INftCollection[];
+  totalRecordCount: number;
+};
+
+export type INftCollectionItem = {
+  alias: string;
+  balance: string;
+  chainId: string;
+  imageLargeUrl: string;
+  imageUrl: string;
+  symbol: string;
+  tokenContractAddress: string;
+  tokenId: string;
+  totalSupply: string;
+};
+export type FetchAccountNftCollectionItemListParams = {
+  symbol: string;
+  caAddresses: string[];
+  caAddressInfos: { chainId: string; caAddress: string }[];
+  skipCount: number;
+  maxResultCount: number;
+  width: number;
+  height: number;
+};
+export type FetchAccountNftCollectionItemListResult = {
+  data: INftCollectionItem[];
+  totalRecordCount: number;
+};
+
+export type FetchTokenPriceParams = {
+  symbols: string[];
+};
+export type FetchTokenPriceResult = {
+  items: { symbol: string; priceInUsd: number }[];
+  totalRecordCount: number;
+};
+
+export interface IAssetsService {
+  fetchAccountTokenList(params: FetchAccountTokenListParams): Promise<FetchAccountTokenListResult>;
+  getSymbolImages(params: GetSymbolImagesParams): Promise<GetSymbolImagesResult>;
+  fetchAccountNftCollectionList(
+    params: FetchAccountNftCollectionListParams,
+  ): Promise<FetchAccountNftCollectionListResult>;
+  fetchAccountNftCollectionItemList(
+    params: FetchAccountNftCollectionItemListParams,
+  ): Promise<FetchAccountNftCollectionItemListResult>;
+  fetchTokenPrice(params: FetchTokenPriceParams): Promise<FetchTokenPriceResult>;
+}
