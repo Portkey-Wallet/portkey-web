a490fbb20249b056b849a872d420a6a91b23f15d
Ian-potter <Ian@portkey.finance>
2023-09-27 10:11:17 +0800

    ci: 🎡 ad test env

--- packages/next-example/rewrites.js
+++ packages/next-example/rewrites.js
@@ -1,11 +1,18 @@
 module.exports = [
     // test1
-    { source: '/api/:path*', destination: 'http://192.168.66.240:5577/api/:path*' },
-    { source: '/connect/:path*', destination: 'http://192.168.66.240:8080/connect/:path*' },
+    // { source: '/api/:path*', destination: 'http://192.168.66.240:5577/api/:path*' },
+    // { source: '/connect/:path*', destination: 'http://192.168.66.240:8080/connect/:path*' },
+    // { source: '/graphql/:path*', destination: 'http://192.168.67.172:8083/AElfIndexer_DApp/PortKeyIndexerCASchema/graphql/:path*' },
 
     // test2
     // { source: '/api/:path*', destination: 'http://192.168.67.51:5577/api/:path*' },
     // { source: '/connect/:path*', destination: 'http://192.168.67.51:8080/connect/:path*' },
+    // { source: '/graphql/:path*', destination: 'http://192.168.67.51:8083/AElfIndexer_DApp/PortKeyIndexerCASchema/graphql/:path*' },
+
+    // test3
+    { source: '/api/:path*', destination: 'http://192.168.66.203:5001/api/:path*' },
+    { source: '/connect/:path*', destination: 'http://192.168.66.203:8001/connect/:path*' },
+    { source: '/graphql/:path*', destination: 'http://192.168.66.203:8083/AElfIndexer_DApp/PortKeyIndexerCASchema/graphql/:path*' },
 
     // mainnet
     // { source: '/api/:path*', destination: 'https://did-portkey.portkey.finance/api/:path*' },
