ebcc0ee93d5d14b87e1d526e78f24c70539f7799
ykx <yangkexin@portkey.finance>
2024-01-08 10:47:07 +0800

    feat: 🎸 use common utils

--- packages/did-ui-react/src/components/GuardianApprovalModal/index.component.tsx
+++ packages/did-ui-react/src/components/GuardianApprovalModal/index.component.tsx
@@ -4,12 +4,10 @@ import CommonBaseModal from '../CommonBaseModal';
 import GuardianApproval from '../GuardianApproval';
 import BackHeader from '../BackHeader';
 import { GuardianApprovalModalProps } from '.';
-import { did, errorTip, handleErrorMessage, setLoading } from '../../utils';
-import { getChainInfo } from '../../hooks';
-import { getVerifierList } from '../../utils/sandboxUtil/getVerifierList';
-import { AccountType, VerifierItem } from '@portkey/services';
+import { errorTip, handleErrorMessage, setLoading } from '../../utils';
 import { UserGuardianStatus } from '../../types';
 import { formatGuardianValue } from '../Guardian/utils/formatGuardianValue';
+import { getGuardianList } from '../SignStep/utils/getGuardians';
 
 const GuardianApprovalModalMain = forwardRef(
   ({
@@ -27,57 +25,15 @@ const GuardianApprovalModalMain = forwardRef(
     onApprovalError,
     operationType,
   }: GuardianApprovalModalProps) => {
-    const verifierMap = useRef<{ [x: string]: VerifierItem }>();
     const [guardianList, setGuardianList] = useState<UserGuardianStatus[]>();
 
-    const getVerifierInfo = useCallback(async () => {
+    const getData = useCallback(async () => {
+      setLoading(true);
       try {
-        const chainInfo = await getChainInfo(originChainId);
-        const list = await getVerifierList({
+        const _guardianList = await getGuardianList({
+          caHash,
+          originChainId,
           sandboxId,
-          chainId: originChainId,
-          rpcUrl: chainInfo?.endPoint,
-          chainType: 'aelf',
-          address: chainInfo?.caContractAddress,
-        });
-        const _verifierMap: { [x: string]: VerifierItem } = {};
-        list.forEach((item: VerifierItem) => {
-          _verifierMap[item.id] = item;
-        }, []);
-        verifierMap.current = _verifierMap;
-      } catch (error) {
-        errorTip(
-          {
-            errorFields: 'getVerifierServers',
-            error: handleErrorMessage(error),
-          },
-          isErrorTip,
-          onApprovalError,
-        );
-      } finally {
-        setLoading(false);
-      }
-    }, [isErrorTip, onApprovalError, originChainId, sandboxId]);
-
-    const getGuardianList = useCallback(async () => {
-      try {
-        if (!caHash) throw 'Please login';
-        const payload = await did.getHolderInfo({
-          caHash: caHash,
-          chainId: originChainId,
-        });
-        const { guardians } = payload?.guardianList ?? { guardians: [] };
-        const guardianAccounts = [...guardians];
-        const _guardianList: UserGuardianStatus[] = guardianAccounts.map((item) => {
-          const key = `${item.guardianIdentifier}&${item.verifierId}`;
-          const _guardian = {
-            ...item,
-            identifier: item.guardianIdentifier,
-            key,
-            guardianType: item.type as AccountType,
-            verifier: verifierMap.current?.[item.verifierId],
-          };
-          return _guardian;
         });
         _guardianList.reverse();
         setGuardianList(_guardianList);
@@ -94,14 +50,7 @@ const GuardianApprovalModalMain = forwardRef(
       } finally {
         setLoading(false);
       }
-    }, [caHash, isErrorTip, onApprovalError, originChainId]);
-
-    const getData = useCallback(async () => {
-      setLoading(true);
-      await getVerifierInfo();
-      await getGuardianList();
-      setLoading(false);
-    }, [getGuardianList, getVerifierInfo]);
+    }, [caHash, isErrorTip, onApprovalError, originChainId, sandboxId]);
 
     useEffect(() => {
       getData();
