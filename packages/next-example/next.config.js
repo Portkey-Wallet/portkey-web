a4315af52b6137ad343b9bf4a921259c76207ed2
ykx <yangkexin@portkey.finance>
2024-01-19 18:22:09 +0800

    feat: 🎸 change next-example13 to next-example

--- packages/next-example/next.config.js
+++ packages/next-example/next.config.js
@@ -2,16 +2,10 @@ const rewrites = require('./rewrites');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return rewrites;
-  },
-};
+    reactStrictMode: false,
+    async rewrites() {
+        return rewrites;
+    },
+}
 
-const withTM = require("next-transpile-modules")([
-  "antd-mobile",
-  "@portkey/did-ui-react",
-]);
-
-
-module.exports = withTM(nextConfig);
+module.exports = nextConfig
