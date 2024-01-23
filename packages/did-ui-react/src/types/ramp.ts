50bb8e70145f86d723e388d7c14ece90c11c27f4
Ian-potter <Ian@portkey.finance>
2024-01-23 14:14:59 +0800

    feat: 🎸 change email input ui

--- packages/did-ui-react/src/types/ramp.ts
+++ packages/did-ui-react/src/types/ramp.ts
@@ -1,6 +1,7 @@
 import { ChainId, IBaseWalletAccount } from '@portkey/types';
 import { AchTxAddressReceivedType } from '@portkey/socket';
 import { CAInfo } from '@portkey/did';
+import { GuardianApprovedItem } from '../components/Guardian/utils/type';
 
 export enum RampTypeEnum {
   BUY = 'BUY',
@@ -84,6 +85,7 @@ export type IRampPreviewInitState = {
   country: string;
   amount: string;
   side: RampTypeEnum;
+  approveList?: GuardianApprovedItem[];
 };
 
 export type ITokenInfo = {
