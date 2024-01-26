ad2d47125157b641d44108dac4ccb5458f7e8b74
Ian-potter <Ian@portkey.finance>
2024-01-26 18:21:18 +0800

    style: 💄 upgraded pointer

--- packages/did-ui-react/src/components/SwitchUpgradedPortkey/index.tsx
+++ packages/did-ui-react/src/components/SwitchUpgradedPortkey/index.tsx
@@ -6,13 +6,13 @@ import './index.less';
 export default function SwitchUpgradedPortkey({ className, onClick }: { className?: string; onClick?: () => void }) {
   return (
     <div className={clsx('portkey-ui-switch-upgraded-wrapper', className)}>
-      <div className="portkey-ui-flex-between-center portkey-ui-switch-upgraded-inner">
+      <div className="portkey-ui-flex-between-center portkey-ui-switch-upgraded-inner" onClick={onClick}>
         <div className="portkey-ui-flex-between-center">
           <img src={Src} />
           <div className="text">Use the Upgraded Portkey</div>
         </div>
 
-        <CustomSvg type="Arrow" onClick={onClick} />
+        <CustomSvg type="Arrow" />
       </div>
     </div>
   );
