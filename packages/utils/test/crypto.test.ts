2f86519404d59e1043a78fe96a52e98dc4e3f2ce
portkey-yellow <yellow.m@portkey.finance>
2024-02-21 17:52:10 +0800

    feat: 🎸 crypto

---
+++ packages/utils/test/crypto.test.ts
@@ -0,0 +1,23 @@
+import { test } from '@jest/globals';
+import { expect } from '@jest/globals';
+import { describe } from 'node:test';
+import { webcrypto } from 'crypto';
+import { WebCryptoManager } from '../src/crypto';
+
+describe('WebCryptoManager', () => {
+  const cryptoManager = new WebCryptoManager(webcrypto.subtle);
+  test('should get keypair', async () => {
+    const keyPair = await cryptoManager.generateKeyPair();
+    expect(keyPair).not.toBeNull();
+    expect(keyPair.publicKey).not.toBeNull();
+    expect(keyPair.privateKey).not.toBeNull();
+  });
+  test('should encrypt and decrypt well', async () => {
+    const data = 'portkey';
+    const keyPair = await cryptoManager.generateKeyPair();
+    const encrypted = await cryptoManager.encrypt(keyPair.publicKey, data);
+    expect(typeof encrypted).toBe('string');
+    const decrypted = await cryptoManager.decrypt(keyPair.privateKey, encrypted);
+    expect(decrypted).toEqual(data);
+  });
+});
