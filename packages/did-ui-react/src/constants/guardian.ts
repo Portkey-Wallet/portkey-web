0efbad6c24efe9443d31dedc512e833a54aa13ae
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-05-11 16:30:06 +0800

    feat: change OfficialWebsite url

--- packages/did-ui-react/src/constants/guardian.ts
+++ packages/did-ui-react/src/constants/guardian.ts
@@ -23,7 +23,7 @@ export const verifierExistTip = 'This verifier has already been used. Please sel
 
 export const MaxVerifierNumber = 100;
 
-export const OfficialWebsite = 'https://test.portkey.finance/entry';
+export const OfficialWebsite = 'https://portkey.finance/entry';
 export const KEY_SHOW_WARNING = 'KEY_SHOW_WARNING';
 export const SHOW_WARNING_DIALOG = '1';
 
