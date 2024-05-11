beaf52b4f12d7c86bc8eafb5e237f483143c532c
peony <ethanael_s@portkey.finance>
2024-05-11 21:35:58 +0800

    fix: insert the Telegram API script

--- packages/did-ui-react/src/constants/telegram.ts
+++ packages/did-ui-react/src/constants/telegram.ts
@@ -49,7 +49,3 @@ export const Open_Login_Guardian_Approval_Bridge: TOpenLoginBridgeURL = {
 };
 
 export const PORTKEY_SDK_TELEGRAM_USER_ID = 'portkey_sdk_telegram_user_id';
-
-export const TELEGRAM_API_SRC = 'https://telegram.org/js/telegram-web-app.js';
-
-export const TELEGRAM_API_SCRIPT_ID = 'telegram-web-app';
