71e7b2682ae252d13760fe1b645d559ffd8e454e
Ian-potter <Ian@portkey.finance>
2024-11-06 21:21:19 +0800

    feat: 🎸 import openlogin

--- packages/did-ui-react/src/components/config-provider/utils.ts
+++ packages/did-ui-react/src/components/config-provider/utils.ts
@@ -4,7 +4,7 @@ import { ISocialLoginConfig, NetworkType, TCustomNetworkType } from '../../types
 
 export const getServiceUrl = () => {
   if (!ConfigProvider.config.serviceUrl) throw Error('Please config serviceUrl');
-  return dealURLLastChar(ConfigProvider.config.serviceUrl);
+  return dealURLLastChar(ConfigProvider.config?.serviceUrl);
 };
 
 export const getSocketUrl = () => {
