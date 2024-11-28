ea33dfa2683488e4cec95e47553fc9d05096282b
kevin-flynn <kevin.flynn@portkey.finance>
2024-11-28 12:18:42 +0800

    chore: set up jest in ESM mode for component testing

--- jest.config.ts
+++ jest.config.ts
@@ -23,6 +23,7 @@ export default {
       {
         diagnostics: false,
         isolatedModules: true,
+        useESM: true,
       },
     ],
   },
@@ -48,7 +49,7 @@ export default {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/node_modules/', '/__generated__/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/__generated__/', './empty.js'],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
@@ -96,19 +97,14 @@ export default {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper,
+  moduleNameMapper: {
+    ...moduleNameMapper,
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/empty.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/empty.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -155,7 +151,7 @@ export default {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
