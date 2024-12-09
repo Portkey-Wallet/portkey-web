75cc1eff06fb1cd0b105f8a48fa38d5dce99845d
sarah-portkey <sarah@portkey.finance>
2024-12-09 19:10:56 +0800

    feat: 🎸 line progress white

--- packages/did-ui-react/src/components/LineProgress/index.tsx
+++ packages/did-ui-react/src/components/LineProgress/index.tsx
@@ -1,4 +1,6 @@
 import { Progress, ProgressProps } from 'antd';
+import { useMemo } from 'react';
+import ConfigProvider from '../config-provider';
 import './index.less';
 
 export type ProgressLineType = {
@@ -6,13 +8,18 @@ export type ProgressLineType = {
 } & ProgressProps;
 
 export default function ProgressLine(props: ProgressLineType) {
+  const theme = useMemo(() => ConfigProvider?.getGlobalConfig()?.theme, []);
+  const [strokeColor, trailColor] = useMemo(() => {
+    return theme === 'dark' ? ['#0996EE', '#303030'] : ['#B8E1FF', '#CDCDCD'];
+  }, [theme]);
+
   return (
     <Progress
       {...props}
       className="portkey-ui-progress-line"
       showInfo={false}
-      strokeColor="#0996EE"
-      trailColor="#303030"
+      strokeColor={strokeColor}
+      trailColor={trailColor}
     />
   );
 }
