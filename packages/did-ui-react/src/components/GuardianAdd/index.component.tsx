484dcf1bc688e2646dcbb6f452dd8838167c900b
amazingDreamCoder <amazingDreamer@portkey.finance>
2024-12-21 05:09:35 +0800

    feat: my-guardian

--- packages/did-ui-react/src/components/GuardianAdd/index.component.tsx
+++ packages/did-ui-react/src/components/GuardianAdd/index.component.tsx
@@ -519,8 +519,13 @@ function GuardianAdd({
       <div className="social input">
         {socialValue?.id ? (
           <div className="portkey-ui-flex-column social-input detail">
-            <span className="social-name">{socialValue?.firstName}</span>
-            <span className="social-email">{socialValue?.isPrivate ? '******' : socialValue?.thirdPartyEmail}</span>
+            <div className="social-input-info">
+              <span className="social-name">{socialValue?.firstName}</span>
+              {(socialValue?.isPrivate || socialValue?.thirdPartyEmail) && (
+                <span className="social-email">{socialValue?.isPrivate ? '******' : socialValue?.thirdPartyEmail}</span>
+              )}
+            </div>
+
             <CustomSvg type="Close2" onClick={handleClearSocialAccount} />
           </div>
         ) : (
