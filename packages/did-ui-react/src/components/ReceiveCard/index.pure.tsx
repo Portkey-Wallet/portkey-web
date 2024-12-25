a3ab773420364060999887fcbe9dde6595bf5b08
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-12-25 10:42:17 +0800

    feat: receive module add pure component

--- packages/did-ui-react/src/components/ReceiveCard/index.pure.tsx
+++ packages/did-ui-react/src/components/ReceiveCard/index.pure.tsx
@@ -1,17 +1,13 @@
-import { useCopyToClipboard } from 'react-use';
+import clsx from 'clsx';
 import CustomSvg from '../CustomSvg';
-import { useEffect, useMemo, useState } from 'react';
-import PortkeyQRCode from '../PortkeyQRCode';
-import { ChainId } from '@portkey/types';
-import { useReceive, useReceiveByETransfer } from '../../hooks/useReceive';
 import { BaseToken } from '../types/assets';
+import { Dispatch, SetStateAction } from 'react';
 import { ChainInfo, ReceiveType, TDepositInfo, TReceiveFromNetworkItem } from '@portkey/services';
+import { ChainId } from '@portkey/types';
+import singleMessage from '../CustomAnt/message';
+import Loading from '../Loading';
+import PortkeyQRCode from '../PortkeyQRCode';
 import AssetModal from '../AssetModal';
-import { MAIN_CHAIN_ID } from '../../constants/network';
-import { usePortkeyAsset } from '../context/PortkeyAssetProvider';
-import { formatStr2EllipsisStr } from '../../utils';
-import CommonButton from '../CommonButton';
-
 import Binance from '../../assets/imgs/binance.png';
 import OKX from '../../assets/imgs/okx.png';
 import Upbit from '../../assets/imgs/upbit.png';
@@ -19,25 +15,58 @@ import BitThumb from '../../assets/imgs/bithumb.png';
 import GateIo from '../../assets/imgs/gate_io.png';
 import Mexc from '../../assets/imgs/mexc.png';
 import Hotcoin from '../../assets/imgs/hotcoin.png';
-import clsx from 'clsx';
+import { CAInfo } from '@portkey/did';
+import { useCopyToClipboard } from 'react-use';
+import CommonButton from '../CommonButton';
 import CommonPromptCard, { PromptCardType } from '../CommonPromptCard';
-import { isNFT } from '../../utils/assets';
+import { TokenItem } from './index.components';
 
-import './index.less';
-import singleMessage from '../CustomAnt/message';
-import Loading from '../Loading';
-
+export interface IPureProps {
+  onBack?: () => void;
+  selectToken: BaseToken;
+  setSelectedType: Dispatch<SetStateAction<string>>;
+  setIsSelectionModalOpen: Dispatch<SetStateAction<boolean>>;
+  selectedDestination: ChainInfo | undefined;
+  selectedSource: TReceiveFromNetworkItem | undefined;
+  loading: boolean;
+  eTransferLoading: boolean;
+  isMainChainToMainChain: boolean;
+  isExchangeSelected: boolean;
+  setIsExchangeSelected: Dispatch<SetStateAction<boolean>>;
+  generateAddress: () =>
+    | {
+        value: string;
+        label: string;
+      }
+    | undefined;
+  caInfo:
+    | {
+        [key: string]: CAInfo;
+      }
+    | undefined;
+  destinationChain: ChainInfo | undefined;
+  receiveType: ReceiveType;
+  currentDepositInfo: TDepositInfo | undefined;
+  renderTip: () => JSX.Element;
+  showExchangeTip: boolean;
+  isSelectionModalOpen: boolean;
+  selectedType: string;
+  renderSelectionList: NetworkItem[] | TReceiveFromNetworkItem[] | (ChainInfo | undefined)[];
+  onSelectedChange: (item: TokenItem) => void;
+  renderSelected: (item: TokenItem | NetworkItem) => JSX.Element | null;
+  isReceivedExchangeModalOpen: boolean;
+  setIsReceivedExchangeModalOpen: Dispatch<SetStateAction<boolean>>;
+}
 enum SELECTION_TYPE {
   SOURCE = 'Source',
   DESITNATION = 'Destination',
   NFT = 'NFT',
 }
-
-enum CHAIN_ID {
-  AELF = 'AELF',
-  tDVV = 'tDVV',
-  tDVW = 'tDVW',
-}
+// enum CHAIN_ID {
+//   AELF = 'AELF',
+//   tDVV = 'tDVV',
+//   tDVW = 'tDVW',
+// }
 
 type NetworkItem = {
   imageUrl: string;
@@ -46,208 +75,35 @@ type NetworkItem = {
   key: string;
 };
 
-export type TokenItem = TReceiveFromNetworkItem | ChainInfo | NetworkItem;
-
-const NETWORK_LIST: NetworkItem[] = [
-  {
-    imageUrl: 'https://portkey-did.s3.ap-northeast-1.amazonaws.com/img/aelf/dappChain.png',
-    chainId: CHAIN_ID.tDVW,
-    name: 'aelf dAppChain',
-    key: 'aelf dAppChain',
-  },
-  {
-    imageUrl: 'https://portkey-did.s3.ap-northeast-1.amazonaws.com/img/aelf/mainChain.png',
-    chainId: CHAIN_ID.tDVV,
-    name: 'aelf MainChain',
-    key: 'aelf MainChain',
-  },
-];
-
-export interface ReceiveCardProps {
-  onBack?: () => void;
-  selectToken: BaseToken;
-}
-
-export default function ReceiveCardMain({ onBack, selectToken }: ReceiveCardProps) {
+export default function ReceiveCardPureComponent(props: IPureProps) {
   const {
+    onBack,
+    selectToken,
+    setSelectedType,
+    setIsSelectionModalOpen,
+    selectedDestination,
+    selectedSource,
     loading,
-    receiveType,
+    eTransferLoading,
+    isMainChainToMainChain,
+    isExchangeSelected,
+    setIsExchangeSelected,
+    generateAddress,
+    caInfo,
     destinationChain,
-    destinationChainList,
-    updateDestinationChain,
-    sourceChain,
-    sourceChainList,
-    setSourceChain,
-  } = useReceive(selectToken);
-
-  const [, setCopied] = useCopyToClipboard();
-  const [isExchangeSelected, setIsExchangeSelected] = useState(selectToken.symbol === 'ELF');
-  const [selectedType, setSelectedType] = useState<string>('');
-  const [selectedSource, setSelectedSource] = useState<TReceiveFromNetworkItem>();
-  const [selectedDestination, setSelectedDestination] = useState<ChainInfo | undefined>();
-  const [isSelectionModalOpen, setIsSelectionModalOpen] = useState(false);
-  const [isReceivedExchangeModalOpen, setIsReceivedExchangeModalOpen] = useState(false);
-  const [currentDepositInfo, setCurrentDepositInfo] = useState<TDepositInfo>();
-  const [{ caInfo }] = usePortkeyAsset();
-
-  const { loading: eTransferLoading, depositInfo } = useReceiveByETransfer({
-    toChainId: destinationChain?.chainId as ChainId,
-    toSymbol: selectToken.symbol,
-    fromNetwork: selectedSource?.network || '',
-    fromSymbol: selectToken.symbol,
     receiveType,
-  });
-
-  const isMainChainToMainChain =
-    selectedSource?.network === MAIN_CHAIN_ID && selectedDestination?.chainId === MAIN_CHAIN_ID;
-
-  useEffect(() => {
-    if (depositInfo) {
-      setCurrentDepositInfo(depositInfo);
-    }
-  }, [depositInfo]);
-
-  useEffect(() => {
-    if (sourceChain && !selectToken.isNFT) {
-      setSelectedSource(sourceChain);
-    }
-
-    if (destinationChain) {
-      setSelectedDestination(destinationChain);
-    }
-
-    if (isMainChainToMainChain && !selectToken.isNFT && selectToken.symbol === 'ELF') {
-      setIsReceivedExchangeModalOpen(true);
-    }
-  }, [sourceChain, destinationChain, isMainChainToMainChain, selectToken.isNFT, selectToken.symbol]);
-
-  const showExchangeTip = useMemo(
-    () =>
-      selectToken?.symbol === 'ELF' &&
-      sourceChain?.network === MAIN_CHAIN_ID &&
-      destinationChain?.chainId !== MAIN_CHAIN_ID,
-    [destinationChain?.chainId, selectToken?.symbol, sourceChain?.network],
-  );
-
-  const onSelectedChange = (item: TokenItem) => {
-    if (selectedType === SELECTION_TYPE.SOURCE) {
-      setSourceChain(item as TReceiveFromNetworkItem);
-      setSelectedSource(item as TReceiveFromNetworkItem);
-      return;
-    }
-    setSelectedDestination(item as ChainInfo);
-
-    if (!selectToken.isNFT) {
-      updateDestinationChain(item as ChainInfo);
-    }
-  };
-
-  const renderSelected = (item: TokenItem | NetworkItem) => {
-    const selection = (
-      <div className="icon-wrapper">
-        <CustomSvg type="Check" fillColor="var(--sds-color-background-default-default)" className="selected-icon" />
-      </div>
-    );
-
-    if (selectedType === SELECTION_TYPE.SOURCE) {
-      if (selectedSource === item) {
-        return selection;
-      }
-    } else {
-      if (
-        selectedDestination === item ||
-        (selectToken.isNFT && selectedDestination?.displayChainName === (item as NetworkItem).name)
-      ) {
-        return selection;
-      }
-    }
-
-    return null;
-  };
-
-  const renderSelectionList = useMemo(() => {
-    if (selectedType === SELECTION_TYPE.NFT) {
-      return NETWORK_LIST;
-    }
-    if (selectedType === SELECTION_TYPE.SOURCE) {
-      return sourceChainList;
-    }
-
-    return destinationChainList || [];
-  }, [destinationChainList, selectedType, sourceChainList]);
-
-  const renderTip = () => {
-    if (selectToken.isNFT) {
-      return (
-        <span>
-          Use this address to receive assets on the <span className="chain">{selectedSource?.name}</span>
-        </span>
-      );
-    }
-
-    if (isMainChainToMainChain && isExchangeSelected) {
-      return (
-        <span>
-          Send {selectToken.symbol} on <span className="chain">{selectedSource?.name}</span> from exchange to this
-          address and receive on the <span className="chain">{destinationChain?.displayChainName}</span>
-        </span>
-      );
-    }
-
-    if (receiveType === ReceiveType.ETransfer) {
-      return (
-        <span>
-          Send {selectToken.symbol} on <span className="chain">{selectedSource?.name}</span> to this address and receive
-          on the <span className="chain">{selectedDestination?.displayChainName}</span>. Transfers from both exchange
-          and non-exchange addresses are accepted.
-        </span>
-      );
-    }
-
-    return (
-      <span>
-        Send {selectToken.symbol} on <span className="chain">{selectedSource?.name}</span> to this address and receive
-        on the <span className="chain">{selectedDestination?.displayChainName}</span>.
-      </span>
-    );
-  };
-
-  const generateAddress = () => {
-    const address = caInfo?.[destinationChain?.chainId as ChainId]?.caAddress;
-    if (currentDepositInfo && selectedSource && !Object.keys(CHAIN_ID).includes(selectedSource?.network)) {
-      return {
-        value: currentDepositInfo.depositAddress,
-        label: formatStr2EllipsisStr(currentDepositInfo.depositAddress, [6, 4]),
-      };
-    }
-
-    if (selectToken.isNFT && selectedSource) {
-      const network = selectedDestination ? selectedDestination?.chainId : selectedSource?.network;
-      return {
-        value: `ELF_${address}_${network}`,
-        label: `ELF_${formatStr2EllipsisStr(address, [4, 4])}_${network}`,
-      };
-    }
-
-    if (address && selectedDestination) {
-      if (
-        isMainChainToMainChain ||
-        !(selectedDestination && Object.keys(CHAIN_ID).includes(selectedDestination?.chainId))
-      ) {
-        if (isExchangeSelected) {
-          return {
-            value: address,
-            label: formatStr2EllipsisStr(address, [6, 4]),
-          };
-        }
-      }
-      return {
-        value: `ELF_${address}_${selectedDestination.chainId}`,
-        label: `ELF_${formatStr2EllipsisStr(address, [4, 4])}_${selectedDestination.chainId}`,
-      };
-    }
-  };
-
+    currentDepositInfo,
+    renderTip,
+    showExchangeTip,
+    isSelectionModalOpen,
+    selectedType,
+    renderSelectionList,
+    onSelectedChange,
+    renderSelected,
+    isReceivedExchangeModalOpen,
+    setIsReceivedExchangeModalOpen,
+  } = props;
+  const [, setCopied] = useCopyToClipboard();
   return (
     <>
       <div className="portkey-ui-receive-content">
---
+++ packages/did-ui-react/src/components/ReceiveList/index.pure.tsx
@@ -0,0 +1,74 @@
+import React, { ChangeEvent } from 'react';
+import PortkeyStyleProvider from '../PortkeyStyleProvider';
+import CustomSvg from '../CustomSvg';
+import clsx from 'clsx';
+import CoinImage from '../CoinImage';
+import { IUserTokenItemNew } from '@portkey/services';
+import CommonInput from '../CommonInput';
+import { SelectTokenType } from '../Asset/index.component';
+import Loading from '../Loading';
+import './index.less';
+
+export interface IPureProps {
+  onBack?: () => void;
+  onInputChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onItemClick: (item: SelectTokenType) => void;
+  isLoading?: boolean;
+  currentTokenList: IUserTokenItemNew[];
+}
+export default function ReceiveListPureComponent(props: IPureProps) {
+  const { onBack, onInputChange, onItemClick, isLoading, currentTokenList } = props;
+  return (
+    <PortkeyStyleProvider>
+      <div className={clsx('portkey-ui-received-list-wrapper')}>
+        <div className="received-list-nav">
+          <div className="left-icon" onClick={onBack}>
+            <CustomSvg type="ArrowLeft" fillColor="var(--sds-color-icon-default-default)" />
+          </div>
+          <div className="received-list-header">
+            <span>Select Asset to Receive</span>
+          </div>
+        </div>
+        <div className="received-list-search">
+          <div className="input-container">
+            <CommonInput type="search" placeholder="Search" onChange={onInputChange} />
+          </div>
+        </div>
+        <div className="received-list-body">
+          <div
+            className="nft-wrapper"
+            onClick={() => {
+              onItemClick({
+                chainId: 'tDVV',
+                symbol: 'ELF',
+                decimals: 0,
+                address: '',
+                isNFT: true,
+              });
+              return;
+            }}>
+            <CustomSvg type="Photo" className="icon" fillColor="var(--sds-color-icon-default-default)" />
+            <div className="label-wrapper">
+              <span>Receive NFTs</span>
+              <CustomSvg type="ChevronRight" fillColor="var(--sds-color-icon-default-default)" />
+            </div>
+          </div>
+          {isLoading ? (
+            <div className="loading-container">
+              <Loading width={32} height={32} />
+            </div>
+          ) : (
+            <div className="token-list">
+              {currentTokenList.map((item: IUserTokenItemNew) => (
+                <div key={item.symbol} className="item" onClick={() => onItemClick(item)}>
+                  <CoinImage symbol={item.symbol} src={item.imageUrl} width={42} />
+                  <span>{item.symbol}</span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </PortkeyStyleProvider>
+  );
+}
